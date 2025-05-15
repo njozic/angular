08f6ccd17bb981d1d934d5ba0278001e0f957e69
Niko Jozic-Zauner <niko.jozic@gmail.com>
2025-05-15 22:37:44 +0200

    added output functionality

--- 01-starting-project/src/app/user/user.component.ts
+++ 01-starting-project/src/app/user/user.component.ts
@@ -1,22 +1,23 @@
-import { Component, Input } from '@angular/core';
-
-
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
-  selector: 'app-user',
-  standalone: true,
-  imports: [],
-  templateUrl: './user.component.html',
-  styleUrl: './user.component.css'
+    selector: 'app-user',
+    imports: [],
+    templateUrl: './user.component.html',
+    styleUrl: './user.component.css'
 })
 
-export class UserComponent { 
-  @Input() avatar!: string;
-  @Input() name!: string;
+export class UserComponent {
+  @Input({required: true}) id!: string; 
+  @Input({required: true}) avatar!: string;
+  @Input({required: true}) name!: string;
+  @Output() select = new EventEmitter();
 
   get imagePath() {
-    return 'assets/users' + this.avatar;
+    return 'assets/users/' + this.avatar;
   }
   
-  onSelectedUser() { }
+  onSelectedUser() { 
+    this.select.emit(this.id);
+  }
 }
