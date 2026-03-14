1ffe31f0149742ee9b9b0cd5a8a7cb00887f5df0
Niko Jozić-Zauner <niko.jozic@gmail.com>
2026-03-14 11:00:26 +0100

    started with module 13

--- src/app/recipe-list/recipe-list.ts
+++ src/app/recipe-list/recipe-list.ts
@@ -1,4 +1,5 @@
 import { Component, signal, computed } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { RecipeModel } from '../models';
 import { MOCK_RECIPES} from '../mock-recipes';
@@ -6,22 +7,37 @@ import { RecipeDetail } from '../recipe-detail/recipe-detail';
 
 @Component({
   selector: 'app-recipe-list',
-  imports: [ RecipeDetail],
+  imports: [ RecipeDetail, FormsModule],
   templateUrl: './recipe-list.html',
   styleUrl: './recipe-list.css',
 })
 export class RecipeList {
 
   protected readonly show = signal(false);
+
+  // Initialize the recipe signal with the first recipe from the mock data
+  // Alternatively, you could initialize it with null and handle that case in the template
   protected recipe = signal<RecipeModel>(MOCK_RECIPES[0]);
+
+  protected recipes = signal<RecipeModel[]>(MOCK_RECIPES);
+  protected searchTerm = signal('');
+
+  protected filteredRecipes = computed(() => {
+    const query = this.searchTerm().trim().toLowerCase();
+    return this.recipes().filter(r => r.name.toLowerCase().includes(query));
+  });
 
   protected logMessage(message: string): void {
     console.log(message);
   }
 
-  protected setRecipe(id: number): void {
-    this.recipe.set(MOCK_RECIPES[id]);
+  protected setRecipe(selectedRecipe: RecipeModel): void {
+    this.recipe.set(selectedRecipe);
     this.show.set(true);
+  }
+
+  protected updateSearch(newVal: string): void{
+    this.searchTerm.set(newVal); 
   }
   
 }
